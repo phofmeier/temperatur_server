9824a8bdba783ba1aa50e4781ab7c55f0182a982
phofmeier <phofmeier@googlemail.com>
2022-11-06 14:28:05 +0100

    update workflows

--- tests/import_test.py
+++ tests/import_test.py
@@ -1,11 +1,18 @@
+import pytest
+
+
 def test_import():
-    import database.database  # noqa: F401
-    import input.measurement_parser  # noqa: F401
-    import models.fourier_series_model  # noqa: F401
-    import models.meat_estimator  # noqa: F401
-    import models.meat_model  # noqa: F401
-    import models.oven_estimator  # noqa: F401
-    import models.oven_model  # noqa: F401
-    import models.predictor  # noqa: F401
-    import simulator.main  # noqa: F401
-    import temperatur_server.main  # noqa: F401
+
+    try:
+        import database.database  # noqa: F401
+        import input.measurement_parser  # noqa: F401
+        import models.fourier_series_model  # noqa: F401
+        import models.meat_estimator  # noqa: F401
+        import models.meat_model  # noqa: F401
+        import models.oven_estimator  # noqa: F401
+        import models.oven_model  # noqa: F401
+        import models.predictor  # noqa: F401
+        import simulator.main  # noqa: F401
+        import temperatur_server.main  # noqa: F401
+    except ImportError:
+        pytest.fail("Import Error occurred. Check dependencies.")
