75cb8813e1e36ce7533b9173bc0aa29ceed76f85
phofmeier <phofmeier@googlemail.com>
2022-11-13 18:35:05 +0100

    move prediction from main to predictor

--- src/database/database.py
+++ src/database/database.py
@@ -2,7 +2,7 @@
 """
 
 import time
-from typing import List, Optional, TypedDict
+from typing import List, Optional, Tuple, TypedDict, Union
 
 import pandas as pd
 
@@ -86,8 +86,8 @@ class MeasurementDB:
         return f"[{temp_1_str} , {temp_2_str} ]"
 
     def getInterpolBetweenTime(
-        self, dt: float, start: str, stop: Optional[str] = None
-    ) -> List[List[float]]:
+        self, dt: float, start: Union[str, int], stop: Optional[str] = None
+    ) -> Tuple[List[float], List[float]]:
         """Get data between to timepoints interpolated to a uniform timegrid.
 
         The data between start and stop is interpolated to a uniform time grid
@@ -113,12 +113,20 @@ class MeasurementDB:
         temp_1_db["DateTime"] = pd.to_datetime(temp_1_db["ts"])
         temp_1_db = temp_1_db.set_index("DateTime")
         temp_1_db = temp_1_db[start_time:stop_time]
-        temp_1_db = temp_1_db["value"].resample(str(dt) + "s", origin=start).mean()
+        temp_1_db = (
+            temp_1_db["value"]
+            .resample(pd.to_timedelta(dt, unit="s"), origin=start)
+            .mean()
+        )
 
         temp_2_db = self.db_2.copy()
         temp_2_db["DateTime"] = pd.to_datetime(temp_2_db["ts"])
         temp_2_db = temp_2_db.set_index("DateTime")
         temp_2_db = temp_2_db[start_time:stop_time]
-        temp_2_db = temp_2_db["value"].resample(str(dt) + "s", origin=start).mean()
+        temp_2_db = (
+            temp_2_db["value"]
+            .resample(pd.to_timedelta(dt, unit="s"), origin=start)
+            .mean()
+        )
 
-        return [temp_1_db.to_list(), temp_2_db.to_list()]
+        return temp_1_db.to_list(), temp_2_db.to_list()
--- src/models/predictor.py
+++ src/models/predictor.py
@@ -1,24 +1,84 @@
+import copy
+import traceback
+from multiprocessing import Process, Queue
+from typing import List, Optional, Tuple, Union
+
+import numpy as np
+
 from models.meat_estimator import MeatEstimator
 from models.oven_estimator import OvenEstimator
 
 
-class Predictor:
-    def __init__(self) -> None:
-        self._oven_est = OvenEstimator()
-        self._meat_est = MeatEstimator()
-        self._dt = 10.0
-        pass
+class PredictionInputData:
+    def __init__(
+        self,
+        outer_measurements: List[float],
+        inner_measurements: List[float],
+        start_time: int,
+        core_end_temp: float,
+    ) -> None:
+        self.outer_measurements = outer_measurements
+        self.inner_measurements = inner_measurements
+        self.start_time = start_time
+        self.core_end_temp = core_end_temp
 
-    def fit(self, outer_measurement, inner_measurement):
+
+class PredictionResult:
+    def __init__(
+        self,
+        timestamps_fit: List[int],
+        oven_temp_fit: List[float],
+        meat_state_fit: List[List[float]],
+        timestamps_prediction: List[int],
+        oven_temp_prediction: List[float],
+        meat_state_prediction: List[List[float]],
+        timestamp_end: int,
+    ) -> None:
+        self.timestamps_fit = timestamps_fit
+        self.oven_temp_fit = oven_temp_fit
+        self.meat_state_fit = meat_state_fit
+        self.timestamps_prediction = timestamps_prediction
+        self.oven_temp_prediction = oven_temp_prediction
+        self.meat_state_prediction = meat_state_prediction
+        self.timestamp_end = timestamp_end
+
+    def to_list(self) -> List[Union[List[int], List[float], List[List[float]], int]]:
+        return [
+            self.timestamps_fit,
+            self.oven_temp_fit,
+            self.meat_state_fit,
+            self.timestamps_prediction,
+            self.oven_temp_prediction,
+            self.meat_state_prediction,
+            self.timestamp_end,
+        ]
+
+
+class Predictor:
+    def __init__(
+        self, dt: float = 10, oven_num_func: int = 10, meat_num_func: int = 10
+    ) -> None:
+        self._oven_est = OvenEstimator(dt, oven_num_func)
+        self._meat_est = MeatEstimator(dt, meat_num_func)
+        self._dt = dt
+        self.process: Optional[Process] = None
+        self.result: Optional[PredictionResult] = None
+        self.input_q: Queue[PredictionInputData] = Queue(maxsize=1)
+        self.output_q: Queue[PredictionResult] = Queue(maxsize=1)
+
+    def fit(
+        self, outer_measurement: List[float], inner_measurement: List[float]
+    ) -> Tuple[Tuple[np.ndarray, float, float, float], Tuple[float, np.ndarray], float]:
         self._oven_params = self._oven_est.fit_params(outer_measurement)
         self._meat_params = self._meat_est.fit_params(
             inner_measurement, outer_measurement
         )
         self._t0 = len(outer_measurement) * self._dt
-        return self._oven_params, self._meat_params
+        return self._oven_params, self._meat_params, self._t0
 
-    def predict(self, ref_temp, max_dur=8 * 10 * 60 * 60):
-        t_0 = self._t0
+    def predict(
+        self, ref_temp: float, t_0: float, max_dur: int = 8 * 10 * 60 * 60
+    ) -> Tuple[List[float], List[List[float]], float]:
         meat_x_0 = self._meat_params[1][-1, :]
         oven_temp = [self._oven_est._model.func(t_0, *self._oven_params)]
         meat_state = [meat_x_0.tolist()]
@@ -36,3 +96,82 @@ class Predictor:
                 break
 
         return oven_temp, meat_state, i * self._dt + t_0
+
+    def run_parallel(self, input_data: PredictionInputData) -> None:
+        self.save_result()
+        if self.is_running():
+            # Todo: throw error
+            print("still alive")
+            return
+        self.input_q.put(input_data)
+        if self.process is not None:
+            self.process.close()
+        self.process = Process(
+            target=self.generate_result,
+            daemon=False,
+        )
+        self.process.start()
+        return
+
+    def is_running(self) -> bool:
+        self.save_result()
+        if self.process is None:
+            return False
+        return self.process.is_alive()
+
+    def maybe_get_result(
+        self,
+    ) -> Optional[List[Union[List[int], List[float], List[List[float]], int]]]:
+        self.save_result()
+        if self.is_running():
+            return None
+
+        if self.result is not None:
+            result_return = copy.deepcopy(self.result)
+            self.result = None
+            return result_return.to_list()
+
+        return None
+
+    def save_result(self) -> None:
+        if not self.output_q.empty():
+            self.result = self.output_q.get()
+
+    def generate_result(self) -> None:
+        try:
+            data = self.input_q.get()
+            start_time = data.start_time
+            oven_params, meat_params, t_0 = self.fit(
+                data.outer_measurements, data.inner_measurements
+            )
+            oven_temp, meat_state, duration_to_end = self.predict(
+                data.core_end_temp, t_0
+            )
+            t_fit = start_time + np.array(
+                np.arange(len(meat_params[1])), dtype=int
+            ) * int(self._dt * 1e9)
+            t_pred = t_fit[-1] + np.array(np.arange(len(oven_temp)), dtype=int) * int(
+                self._dt * 1e9
+            )
+            t_oven = (
+                np.array(np.arange(meat_params[1].shape[0]), dtype=float) * self._dt
+            )
+            t_end = start_time + int(duration_to_end * 1e9)
+            oven_fit = []
+            for i in range(t_oven.shape[0]):
+                oven_fit.append(self._oven_est._model.func(t_oven[i], *oven_params))
+
+            result = [
+                t_fit.tolist(),
+                oven_fit,
+                meat_params[1].tolist(),
+                t_pred.tolist(),
+                oven_temp,
+                meat_state,
+                t_end,
+            ]
+            self.output_q.put(PredictionResult(*result))
+
+        except BaseException as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+            traceback.print_exc()
