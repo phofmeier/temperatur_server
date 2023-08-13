9c200fe3ad8c1c2e937f38c73dd19bd100641125
phofmeier <phofmeier@googlemail.com>
2023-08-13 17:44:15 +0200

    update python version

--- tests/import_test.py
+++ tests/import_test.py
@@ -12,6 +12,9 @@ def test_import():
         import models.oven_model  # noqa: F401
         import models.predictor  # noqa: F401
         import simulator.main  # noqa: F401
+        import temperatur_server.api.api_manager  # noqa: F401
+        import temperatur_server.api.js_api  # noqa: F401
         import temperatur_server.main  # noqa: F401
+        import temperatur_server.server_state  # noqa: F401
     except ImportError:
         pytest.fail("Import Error occurred. Check dependencies.")
